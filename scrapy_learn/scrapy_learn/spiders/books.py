b03df09df25a82b82b4a1b4dda68b5114293728f
Islam <aissiislam97@gmail.com>
2022-08-06 17:46:49 +0100

    [BOOKS] grapping urls with scrapy

--- scrapy_learn/scrapy_learn/spiders/books.py
+++ scrapy_learn/scrapy_learn/spiders/books.py
@@ -1,45 +1,21 @@
-from time import sleep
 from scrapy import Spider
-from selenium import webdriver
-from scrapy.selector import Selector
 from scrapy.http import Request
-import os
-
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 
 
 class BooksSpider(Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
 
-    def start_requests(self):
-        self.driver = webdriver.Chrome(
-            f"{os.getcwd()}/scrapy_learn/assets/chromedriver"
-        )
-        self.driver.get("https://books.toscrape.com/")
-        selector = Selector(text=self.driver.page_source)
-        books = selector.xpath("//h3/a/@href").extract()
+    def parse(self, response):
+        books = response.xpath("//h3/a/@href").extract()
         for book in books:
-            url = "https://books.toscrape.com/" + book
-            yield Request(url, callback=self.parse_book)
+            absolute_url = response.urljoin(book)
+            yield Request(absolute_url, callback=self.parse_book)
+        # ? go to the next page
+        next_page_url = response.xpath('//a[text()="next"]/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
 
-        while True:
-            try:
-                next_page = self.driver.find_element(By.XPATH, '//a[text()="next"]')
-                sleep(3)
-                self.logger.info("Getting the next page ...")
-                next_page.click()
-
-                selector = Selector(text=self.driver.page_source)
-                books = selector.xpath("//h3/a/@href").extract()
-                for book in books:
-                    url = "https://books.toscrape.com/catalogue/" + book
-                    yield Request(url, callback=self.parse_book)
-            except NoSuchElementException:
-                self.logger.info("All pages were succefully scraped.")
-                self.driver.quit()
-                break
-
-    def parse_book(self, reponse):
+    def parse_book(self, response):
         pass
