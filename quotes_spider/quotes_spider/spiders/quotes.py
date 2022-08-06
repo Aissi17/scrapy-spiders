2b0f7b072e0314985405f5e5a569cbdda12b0a11
Islam <aissiislam97@gmail.com>
2022-08-06 12:28:43 +0100

    added spider to scrape quotes

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -2,9 +2,11 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    name = "quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        h1_text = response.xpath("//h1/a/text()").extract_first()
+        all_tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        yield {"H1 text": h1_text, "Tags": all_tags}
