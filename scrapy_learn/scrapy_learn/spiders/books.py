747a5581c1dad0fff17a0561ae0e0f99b943b8b2
Islam <aissiislam97@gmail.com>
2022-08-06 15:26:25 +0100

    [BOOKS] set up the spider

--- scrapy_learn/scrapy_learn/spiders/books.py
+++ scrapy_learn/scrapy_learn/spiders/books.py
@@ -1,10 +1,14 @@
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class BooksSpider(scrapy.Spider):
-    name = 'books'
-    allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
+class BooksSpider(CrawlSpider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["http://books.toscrape.com/"]
+    rules = [
+        Rule(LinkExtractor(), callback="parse", follow=False),
+    ]
 
     def parse(self, response):
         pass
