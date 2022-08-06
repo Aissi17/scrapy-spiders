ebdb51912611669ad9625e4602bfa950593c3d39
Islam <aissiislam97@gmail.com>
2022-08-06 14:40:25 +0100

    added pagination

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -7,11 +7,13 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
-        result = []
         quotes = response.xpath('//*[@class="quote"]')
         for quote in quotes:
             text = quote.xpath('.//*[@class="text"]/text()').extract_first()
             author = quote.xpath('.//*[@class="author"]/text()').extract_first()
             tags = quote.xpath('.//*[@class="tag"]/text()').extract()
-            result.append({"text": text, "author": author, "tags": tags})
-        yield {"Quotes": result}
+            yield ({"text": text, "author": author, "tags": tags})
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        absolute_next_url = response.urljoin(next_page_url)
+
+        yield scrapy.Request(absolute_next_url)
